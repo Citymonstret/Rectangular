320729d7221bfd26dc22a3fc491c265a3c111a4c
Sauilitired <citymonstret@hotmail.com>
2016-06-14 17:04:12 +0200

    Add a metadata system

--- src/main/java/com/intellectualsites/rectangular/data/RegionData.java
+++ src/main/java/com/intellectualsites/rectangular/data/RegionData.java
@@ -1,7 +1,92 @@
 package com.intellectualsites.rectangular.data;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class RegionData {
 
+    /*
+    Meta Data Types:
+    @f = Flag
+    @m = Member
+    @b = Blocked User
+    @d = Data (general meta)
+     */
 
+    @RequiredArgsConstructor
+    public enum DataType {
+        FLAG            ("@f"),
+        MEMBER          ("@m"),
+        BLOCKED         ("@b"),
+        DATA            ("@d")
+        ;
+        @Getter
+        private final String key;
+    }
+
+    @RequiredArgsConstructor
+    public static class DataEntry {
+        @Getter
+        private final DataType dataType;
+        @Getter
+        private final String key;
+        @Getter
+        private final String value;
+    }
+
+    @Getter
+    private String owner;
+
+    private Map<DataType, Map<String, DataEntry>> dataEntries = new ConcurrentHashMap<>();
+    {
+        for (DataType type : DataType.values()) {
+            dataEntries.put(type, new HashMap<>());
+        }
+    }
+
+    public RegionData(String owner, String packedData) {
+        this.owner = owner;
+        String[] parts = packedData.split(";");
+        for (String s : parts) {
+            String[] split = s.split(":");
+            String key = split[0];
+            DataType type = getDataType(key);
+            dataEntries.get(type).put(key, new DataEntry(type, key, split[1]));
+        }
+    }
+
+    public static DataType getDataType(String s) {
+        for (DataType t : DataType.values()) {
+            if (s.startsWith(t.getKey())) {
+                return t;
+            }
+        }
+        return DataType.DATA;
+    }
+
+    public DataEntry getDataEntry(DataType type, String key) {
+        return dataEntries.get(type).get(key);
+    }
+
+    public DataEntry getDataEntry(String key) {
+        return getDataEntry(getDataType(key), key);
+    }
+
+    public String getPackedData() {
+        StringBuilder builder = new StringBuilder();
+        for (Map<String, DataEntry> entries : dataEntries.values()) {
+            for (DataEntry entry : entries.values()) {
+                builder.append(entry.getKey()).append(":")
+                        .append(entry.getValue()).append(";");
+            }
+        }
+        return builder.toString();
+    }
 
 }
